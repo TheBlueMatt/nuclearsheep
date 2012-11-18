90b67bb433fdb32deb0bfacd82614deeea36b712
Matt Corallo <git@bluematt.me>
2012-11-18 10:56:44 -0500

    GPL'd (update openshift)

--- nuclearsheep/src/org/hatchunc/nuclearsheep/NuclearSheep.java
+++ nuclearsheep/src/org/hatchunc/nuclearsheep/NuclearSheep.java
@@ -1,12 +1,13 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * NuclearSheepUI.java
+ * Copyright Matt Corallo and Colin Arnott 2012
+ * 
+ * This file is part of nuclearsheep.
  *
- * Created on 17/11/2012, 9:49:43 PM
+ * nuclearsheep is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * nuclearsheep is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see http://www.gnu.org/licenses/.
  */
 package org.hatchunc.nuclearsheep;
 
--- openshift/php/index.php
+++ openshift/php/index.php
@@ -7,5 +7,7 @@
 </head>
 <body>
   This is nuclearsheep, you probably want to check out our <a href="https://github.com/TheBlueMatt/nuclearsheep">GitHub</a>.
+  To install, you need libpcap (get this from your local package manager), <a href=nuclearsheep.jar>the jar</a> and the <a href=libjpcap.so>libjpcap.so</a> file.
+  To run, run with (as root) with java -jar -Djava.library.path=PATH_TO_LIBJPCAP_DIR nuclearsheep.jar
 </body>
 </html>
