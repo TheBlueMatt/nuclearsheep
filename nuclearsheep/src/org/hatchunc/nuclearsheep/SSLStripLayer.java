a72112cfaa9839ab96955532f9062af473dd8354
Matt Corallo <git@bluematt.me>
2012-11-19 12:44:55 -0500

    rm useless import

--- nuclearsheep/src/org/hatchunc/nuclearsheep/SSLStripLayer.java
+++ nuclearsheep/src/org/hatchunc/nuclearsheep/SSLStripLayer.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
