543f95029e7c4bc27f66143e7e23ed26567a718d
Matt Corallo <git@bluematt.me>
2012-11-19 23:17:48 -0500

    Update to openshift

--- openshift/php/index.php
+++ openshift/php/index.php
@@ -8,8 +8,9 @@
 <body>
   <h2>NuclearSheep</h2>
   <img src=Sheep_icon_05.jpg alt="SHEEP" /><br>
-  This is nuclearsheep, you probably want to check out our <a href="https://github.com/TheBlueMatt/nuclearsheep">GitHub</a>.<br>
-  Before you can run, you need libpcap, sslstrip (get these from your local package manager) and the Jpcap package for your OS (see <a href="http://netresearch.ics.uci.edu/kfujii/Jpcap/doc/download.html">their download page</a>).<br>
-  Grab <a href=nuclearsheep.jar>the jar</a> and run (as root) with java -jar nuclearsheep.jar
+  This is a program designed to show off how rediculously easy simple SSLStrip-based attacks are to perform against many popular websites (including many banks!).<br>
+  Before you can run it, you need libpcap, sslstrip (get these from your local package manager) and the Jpcap package for your OS (see <a href="http://netresearch.ics.uci.edu/kfujii/Jpcap/doc/download.html">their download page</a>).<br>
+  Grab <a href=nuclearsheep.jar>the jar</a> and run (as root) with java -jar nuclearsheep.jar<br>
+  You can find a copy of the code at <a href="https://github.com/TheBlueMatt/nuclearsheep">GitHub</a>.<br>
 </body>
 </html>
